f523f35b5f59e5664bc8c720a337237607143a33
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2018-04-01 20:07:20 +0200

    Removed init script and composer from production-ready apache image

--- 7.1/apache/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-composer install --no-interaction \
-        --no-progress \
-        --no-scripts \
-        --no-suggest \
-        --prefer-dist \
-        --no-dev \
-&& apache2-foreground
